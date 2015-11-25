55d034c5ce77a58f5d4e581a9b0ef706853441d4
queicherius <queicherius@gmx.de>
2015-11-25 18:40:47 +0100

    Handle API errors

--- update.py
+++ update.py
@@ -1,10 +1,16 @@
 import json, requests, subprocess
 
-api_url = 'http://gw2profits.com/json/v2/forge/'
+api_url = 'http://gw2profits.com/json/v2/forrge/'
 file_name = 'recipes.json'
 
 # Grab the recipes from the API
 resp = requests.get(url=api_url)
+
+if resp.status_code != 200:
+    print 'API error: Response has status != 200'
+    exit()
+
+# Parse the JSON
 data = json.loads(resp.text)
 
 # Write them formatted into a file
