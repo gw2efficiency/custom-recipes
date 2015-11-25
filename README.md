fd82aa8dee4342565531f38437d910e2b9ee051d
queicherius <queicherius@gmx.de>
2015-11-25 18:31:00 +0100

    Update script and readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# gw2-mystic-forge-recipes
-Automatic diffs for mystic forge recipes managed by Mystic.5934
+# Mystic forge recipes for GW2
+
+These are automatically generated diffs for mystic forge recipes managed by Mystic.5934
+via [this forum thread](https://forum-en.guildwars2.com/forum/community/api/Created-a-json-file-for-Mystic-Forge-recipes/first#post5795657).
+
+The script runs every 30 minutes and commits into this repository if something changed.
+This makes it a little easier to follow what exactly changed and finding possible bugs.
---
+++ update.py
@@ -0,0 +1,24 @@
+import json, requests, subprocess
+
+api_url = 'http://gw2profits.com/json/v2/forge/'
+file_name = 'recipes.json'
+
+# Grab the recipes from the API
+resp = requests.get(url=api_url)
+data = json.loads(resp.text)
+
+# Write them formatted into a file
+with open(file_name, 'w') as outfile:
+    json.dump(data, outfile, indent=4, sort_keys=True)
+
+# Check if the file changed
+diff = subprocess.Popen("git diff " + file_name, shell=True, stdout=subprocess.PIPE).stdout.read()
+
+# If yes, commit and push it to the repo, else do nothing
+if len(diff) > 0:
+    print "JSON file changed, committing..."
+    print subprocess.Popen("git commit " + file_name + " -m \"[Automatic] Update JSON file\"", shell=True, stdout=subprocess.PIPE).stdout.read()
+    print "Committed... pushing"
+    print subprocess.Popen("git push", shell=True, stdout=subprocess.PIPE).stdout.read()
+else:
+    print "JSON file is the same"
